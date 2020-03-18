77eab551bd0aa69b7092c455d9740392aa7a7f33
Norton Li <nli@zipcodes-MBP.home>
2020-03-17 20:00:16 -0400

    Lab Updated

--- gradebook.py
+++ gradebook.py
@@ -1,67 +1,87 @@
 import uuid
-import Enum
+from enum import Enum
+from typing import List, Dict
 
 class AliveStatus(Enum):
-        Decreased = 0
-        Alive = 1
+    Deceased = 0
+    Alive = 1
 
-class Person():
-        def __init__(self, first_name, last_name, dob, alive):
-            self.first_name = first_name
-            self.last_name = last_name
-            self.dob = dob
-            self.alive = alive
+class Person:
+    def __init__(self, first_name, last_name, dob):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.dob = dob
+        self.alive = AliveStatus.Alive
 
-        def update_first_name(self, new_f_n):
-            self.first_name = new_f_n
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
-        def update_last_name(self, new_l_n):
-            self.last_name = new_l_n
+    def update_first_name(self, first_name):
+        self.first_name = first_name
 
-        def update_dob(self, new_d):
-            self.dob = new_d
+    def update_last_name(self, last_name):
+        self.last_name = last_name
 
-        def update_status(self, new_s):
-            self.alive = new_s
+    def update_dob(self, dob:str):
+        self.dob = dob
+
+    def update_status(self, alive: AliveStatus):
+        self.alive = alive
 
 
 class Instructor(Person):
-        def __init__(self, instructor_id):
-            self.instructor_id = "Instructor_"uuid.uuid4()
-            super().__init__(first_name, last_name, dob, alive)
+    def __init__(self, first_name, last_name, dob):
+        super().__init__(first_name, last_name, dob)
+        self.instructor_id = f"Instructor_{uuid.uuid4()}"
+
 
 class Student(Person):
-        def __init__(self, student_id):
-            self.student_id = "Student_"uuid.uuid4()
-            super.__init__(first_name, last_name, dob, alive)
+    def __init__(self, first_name, last_name, dob):
+        super().__init__(first_name, last_name, dob)
+        self.student_id = f"Student_{uuid.uuid4()}"
+
 
 class ZipCodeStudent(Student):
-        def __init__(self):
-            super.__init__(first_name, last_name, dob, alive)
+    def __init__(self, first_name, last_name, dob):
+        super().__init__(first_name, last_name, dob)
 
-class College_Student(Student):
-        def __init__(self):
-            super.__init__(first_name, last_name, dob, alive)
 
-class Classroom():
-        def __init__(self, students, instructors):
-            self.students = students
-            self.instrutors = instructors
+class CollegeStudent(Student):
+    def __init__(self, first_name, last_name, dob):
+        super().__init__(first_name, last_name, dob)
 
-        def add_instructor(a_i):
-            pass
 
-        def remove_instructor(r_i):
-            pass
+class GradeBook:
+    pass
 
-        def add_student(a_s):
-            pass
 
-        def remove_student(r_s):
-            pass
+class Classroom:
+    def __init__(self):
+        self.students: Dict[str, Student] = {}
+        self.instructors: Dict[str, Instructor] = {}
 
-        def print_instructor(p_i):
-            pass
+    def add_instructor(self, instructor: Instructor):
+        self.instructors[instructor.instructor_id] = instructor
 
-        def print_student(p_s):
-            pass
+    def remove_instructor(self, instructor:Instructor):
+        if instructor.instructor_id in self.instructors:
+            del self.instructors[instructor.instructor_id]
+
+    def add_student(self, student: Student):
+        self.students[student.student_id] = student
+
+    def remove_student(self, student):
+        if student.student_id in self.students:
+            del self.students[student.student_id]
+
+    def print_instructors(self):
+        for key, value in self.instructors.items():
+            print(f"{key}: {value}")
+
+    def print_students(self):
+        for key, value in self.students.items():
+            print(f"{key}: {value}")
+
+
+if __name__ == "__main__":
+    pass
